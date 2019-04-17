49b8011afa29848ff5f61e6bded22cb1b75ddf01
2wrds <49554558+2wrds@users.noreply.github.com>
2019-04-17 16:19:26 +0300

    Исправлен слик

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
+	$('#phone').mask("+7 (999) 999-99-99");
+});
+
+btn_start.onclick = function(){
 	$('.slider').slick({
 		arrows: false,
 		dots: true,
 	  	dotsClass: 'slider-circles'
 	});
-	
-	$('#phone').mask("+7 (999) 999-99-99");
-});
+
+	$('.slider').slick('setPosition');
+};
